f846d5a2afd34e887be9a11282fb338b1435efbb
A2Pro <87340157+A2Pro@users.noreply.github.com>
2024-05-17 19:30:19 -0700

    Added leaflet, made sure previously added coords don't get re-added

--- get_coords.py
+++ get_coords.py
@@ -2,15 +2,14 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv
+load_dotenv()
+
 API_KEY = os.getenv("GEOIPLOCATOR_API_KEY")
 
-
 def get_coords(ip):
-    response = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey=7f52e57f64904bcaa6c5bca14fd0a27f&ip={ip}')
+    response = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey={API_KEY}&ip={ip}')
     json = response.json()
     lat = 0
-    print(json)
     long = 0
     try:
         lat = json["latitude"]
@@ -23,23 +22,19 @@ def main():
     x = 0
     y = 0
     ips = []
+    with open("coordinates.txt", "r") as file:
+        coords_list = file.read()
     with open('ip_addresses.txt') as file:
-        print()
         ips = [line.strip() for line in file.readlines()]
-    
-   
-    for ip in ips:
+    filtered_ips = [ip for ip in ips if ip not in coords_list]
+    for ip in filtered_ips:
         coords = get_coords(ip)
-        y+=1
-        if(coords["lat"]!=0 or coords["lng"]!=0):
-            x+=1
+        y += 1
+        if coords["lat"] != 0 or coords["lng"] != 0:
+            x += 1
             with open("coordinates.txt", "a") as file:
                 file.write(f'{ip} | {coords["lat"]} | {coords["lng"]} \n')
             print(f'{x} found in {y}')
-            
-        
-        
 
-main()
-
-# we can get rid of development once I get a google maps API key, / implement it (i already have one)
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,45 +1,17 @@
-import requests
 import os
+from flask import Flask, render_template
 from dotenv import load_dotenv
 
-load_dotenv
-API_KEY = os.getenv("GEOIPLOCATOR_API_KEY")
+app = Flask(__name__)
 
+@app.route("/map")
+def map():
+    coordinates = []
+    with open("coordinates.txt") as file:
+        for line in file:
+            ip, lat, lng = line.strip().split(" | ")
+            coordinates.append({"ip": ip, "lat": lat, "lng": lng})
+    return render_template("index.html", coordinates=coordinates)
 
-def get_coords(ip):
-    response = requests.get(f'https://api.ipgeolocation.io/ipgeo?apiKey=7f52e57f64904bcaa6c5bca14fd0a27f&ip={ip}')
-    json = response.json()
-    lat = 0
-    print(json)
-    long = 0
-    try:
-        lat = json["latitude"]
-        long = json["longitude"]
-    except:
-        pass
-    return {"lat": lat, "lng": long}
-
-def main():
-    x = 0
-    y = 0
-    ips = []
-    with open('ip_addresses.txt') as file:
-        print()
-        ips = [line.strip() for line in file.readlines()]
-    
-   
-    for ip in ips:
-        coords = get_coords(ip)
-        y+=1
-        if(coords["lat"]!=0 or coords["lng"]!=0):
-            x+=1
-            with open("coordinates.txt", "a") as file:
-                file.write(f'{ip} | {coords["lat"]} | {coords["lng"]} \n')
-            print(f'{x} found in {y}')
-            
-        
-        
-
-main()
-
-# we can get rid of development once I get a google maps API key, / implement it (i already have one)
+if __name__ == "__main__":
+    app.run(debug=True)
